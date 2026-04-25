89f161318edc8d7482f389338946fe82431424c1
Luke W. Johnston <lwjohnst@gmail.com>
2026-04-25 14:24:03 +0200

    test: ✅ update test script to match changes

--- test-template.sh
+++ test-template.sh
@@ -1,21 +1,23 @@
 #!/usr/bin/env bash
 
-# Needs two arguments:
+# Needs these arguments:
 #
-# 1. type: 'r' or 'general'
+# 1. for_rostools: 'true' or 'false'
+# 2. type: 'r' or 'general'
 
 # Argument naming -----
-type="${1}"
+for_rostools="${1}"
+type="${2}"
 
 if [ -z "$type" ]; then
-  echo "Usage: sh $0 <type>"
-  echo "Example: sh $0 r"
+  echo "Usage: sh $0 <for_rostools> <type>"
+  echo "Example: sh $0 true r"
   exit 1
 fi
 
 # Set up variables and functions for the test -----
-test_name="test-workshop"
-test_dir="$(pwd)/_temp/$type/$test_name"
+test_name="$for_rostools-$type"
+test_dir="$(pwd)/_temp/auto/$test_name"
 template_dir="$(pwd)"
 
 # Needs three arguments:
@@ -23,18 +25,13 @@ template_dir="$(pwd)"
 # 1. Template directory
 # 2. Destination directory
 copy () {
-  # vcs-ref means the current commit/head. This includes uncommitted changes.
+  # '-r HEAD' means use the HEAD, including uncommitted changes.
   uvx copier copy $1 $2 \
     -r HEAD \
     --defaults \
-    --data workshop_type="$type" \
-    --data github_user="first-last" \
-    --data author_given_name="First" \
-    --data author_family_name="Last" \
-    --data github_board_number=22 \
-    --overwrite \
-    --skip-tasks \
-    --trust
+    --data for_rostools=$for_rostools \
+    --data workshop_type=$type \
+    --overwrite
 }
 
 # Pre-test setup -----
@@ -53,19 +50,17 @@ echo "Testing copy for new projects when: 'type'='$type' -----------"
     git add . &&
     git commit --quiet -m "test: initial copy" &&
     # Check that recopy works -----
-    echo "Testing recopy when: 'type'='$type' -----------" &&
-    rm .cz.toml &&
+    echo "Testing recopy when: 'for_rostools'='$for_rostools' and 'type'='$type' -----------" &&
+    rm .gitignore &&
     git add . &&
     git commit --quiet -m "test: preparing to recopy from the template" &&
     uvx copier recopy \
       -r HEAD \
       --defaults \
-      --overwrite \
-      --skip-tasks \
-      --trust &&
+      --overwrite &&
     # Check that copying onto an existing website works -----
-    echo "Testing copy in existing projects when: 'type'='$type' -----------" &&
-    rm .cz.toml .copier-answers.yml &&
+    echo "Testing copy in existing projects when: 'for_rostools'='$for_rostools' and 'type'='$type' -----------" &&
+    rm .gitignore .copier-answers.yml &&
     git add . &&
     git commit --quiet -m "test: preparing to copy onto an existing website" &&
     copy $template_dir $test_dir
